819748dbb758406484e3beba30a017b834af6262
khilesh55 <61207468+khilesh55@users.noreply.github.com>
2021-04-09 09:05:50 -0600

    Updates

---
+++ SaveInputDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpasticityClient
+{
+    /// <summary>
+    /// Interaction logic for SaveInputDialog.xaml
+    /// </summary>
+    public partial class SaveInputDialog : UserControl
+    {
+        public SaveInputDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
